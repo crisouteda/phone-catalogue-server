97db131e2eb51b111598c7b33e22374f18873110
cristina <cristina.outeda.rua@gmail.com>
2022-04-09 13:24:26 +0200

    fixed typo in route

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use(require("./routes/index"));
-app.use("/phone", require("./routes/phones"));
+app.use("/phones", require("./routes/phones"));
 
 // Starting
 app.set("port", process.env.PORT || 4000);
