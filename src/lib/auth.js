e192339664f1027d8d2281870febce1a6e2466ec
cristina <cristina.outeda.rua@gmail.com>
2022-04-16 17:33:26 +0200

    improved error message

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,7 +4,7 @@ module.exports = {
   isLoggedIn(req, res, next) {
     const token = req.body.token;
     if (!token) {
-      res.send({ message: "Sign up to get an authorization token" });
+      res.json({ message: "Sign up to get an authorization token" });
     } else {
       jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
         if (err) {
