d70d660aedeb56bebf329ef52be106644b3a444b
cristina <cristina.outeda.rua@gmail.com>
2022-04-17 22:42:21 +0200

    refactored folders for auth and phones

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,85 +1,13 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
 const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
-const AWS = require("aws-sdk");
-const { isLoggedIn } = require("../lib/auth");
+const { signUp, signIn } = require("../controllers/auth");
 
 const router = express.Router();
-
-const docClient = new AWS.DynamoDB.DocumentClient();
-const env = process.env.STAGE_ENVIRONMNET;
-const TableName = `users-${env}`;
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.post("/signup", async (req, res) => {
-  const params = {
-    TableName,
-  };
-  const { password, email } = req.body;
-  bcrypt.hash(password, 10, (err, hash) => {
-    if (err) {
-      console.log(err);
-    }
-    params["Item"] = { email, password: hash };
-    docClient.put(params, function (err, data) {
-      if (err) {
-        console.error(
-          "Unable to add item. Error JSON:",
-          JSON.stringify(err, null, 2)
-        );
-        res.send(new Error(err));
-      } else {
-        const token = jwt.sign({ email }, process.env.TOKEN_SECRET, {
-          expiresIn: 600,
-        });
-        res.json({
-          message: `Item created successfully. ${JSON.stringify(params)}.`,
-          auth: true,
-          token: token,
-        });
-      }
-    });
-  });
-});
-
-router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-  console.log({ email, password });
-  const params = {
-    TableName,
-    KeyConditionExpression: `email = :email`,
-    ExpressionAttributeValues: {
-      ":email": email,
-    },
-    Limit: 1,
-  };
-  docClient.query(params, function (err, data) {
-    if (err) {
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-    } else {
-      console.log(data.Items);
-      if (data.Items.length > 0) {
-        bcrypt.compare(password, data.Items[0].password, (error, response) => {
-          if (response) {
-            const token = jwt.sign({ email }, process.env.TOKEN_SECRET, {
-              expiresIn: 600,
-            });
-            res.json({ auth: true, token: token }); //everything is sent (even password!!!)
-          } else {
-            res.send({
-              auth: false,
-              message: "wrong user-password combination",
-            });
-          }
-        });
-      } else {
-        res.send({ auth: false, message: "No user exist" });
-      }
-    }
-  });
-});
+router.post("/signup", signUp);
+router.post("/signin", signIn);
 
 module.exports = router;
--- src/routes/phones.js
+++ src/routes/phones.js
@@ -1,182 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const AWS = require("aws-sdk");
-const cors = require("cors");
-const { v4: uuidv4 } = require("uuid");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const { isLoggedIn } = require("../lib/auth");
+const {
+  getAll,
+  getAllPagination,
+  getPhoneById,
+  createPhone,
+  deletePhoneById,
+  updatePhone,
+} = require("../controllers/phone");
 
 const router = express.Router();
-
-const docClient = new AWS.DynamoDB.DocumentClient();
-const env = process.env.STAGE_ENVIRONMNET;
-const TableName = `phone-data-${env}`;
 
 router.use(express.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get("/", async (req, res) => {
-  var params = {
-    TableName,
-    ProjectionExpression: "id, #n, thumbnailFileName, price",
-    ExpressionAttributeNames: { "#n": "name" },
-  };
-  console.log("Scanning Phone table.");
-  docClient.scan(params, onScan);
-
-  function onScan(err, data) {
-    if (err) {
-      console.error(
-        "Unable to scan the table. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-      res.send(new Error(err));
-    } else {
-      console.log("Scan succeeded.", JSON.stringify(data, null, 2));
-      res.send(data.Items);
-    }
-  }
-});
-
-router.get("/pagination/:items/:exclusiveStartKey?", async (req, res) => {
-  const ExclusiveStartKey = req.params.exclusiveStartKey && {
-    id: req.params.exclusiveStartKey,
-  };
-  const Limit = parseInt(req.params.items);
-
-  const params = {
-    TableName,
-    ProjectionExpression: "id, #n, thumbnailFileName, price",
-    ExpressionAttributeNames: { "#n": "name" },
-    KeyConditionExpression: "*",
-    Limit,
-    ExclusiveStartKey,
-  };
-
-  docClient.scan(params, onScan);
-
-  function onScan(err, data) {
-    if (err) {
-      console.error(
-        "Unable to query the table. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-      res.send(new Error(err));
-    } else {
-      console.log("Query succeeded.");
-      const limitReached = Limit > data.Count;
-      res.send({
-        newItems: data.Items,
-        lastEvaluatedKey: !limitReached && data.LastEvaluatedKey.id,
-        limitReached,
-      });
-    }
-  }
-});
-
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const params = {
-    TableName,
-    KeyConditionExpression: `id = :id`,
-    ExpressionAttributeValues: {
-      ":id": req.params.id,
-    },
-    Limit: 1,
-  };
-
-  docClient.query(params, function (err, data) {
-    if (err) {
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-      res.send(new Error(err));
-    } else {
-      console.log(`Query succeeded. Returned item with id=${id}`);
-      res.send(data.Items);
-    }
-  });
-});
-
-router.post("/", isLoggedIn, async (req, res) => {
-  const { data } = req.body;
-  const params = {
-    TableName,
-    Item: { ...JSON.parse(data), id: uuidv4() },
-  };
-  docClient.put(params, function (err, data) {
-    if (err) {
-      console.error(
-        "Unable to add item. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-      res.send(new Error(err));
-    } else {
-      console.log("Added item:", params.Item.id);
-      res.send({
-        message: `Item created successfully.`,
-        id: params.Item.id,
-      });
-    }
-  });
-});
-
-router.post("/delete", isLoggedIn, async (req, res) => {
-  const params = {
-    TableName,
-    Key: { id: req.body.id },
-  };
-
-  docClient.delete(params, function (err, data) {
-    if (err) {
-      console.error(
-        "Unable to delete item. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-      res.send(new Error(err));
-    } else {
-      console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
-      res.send({
-        message: `Item with id=${req.body.id} deleted successfully.`,
-      });
-    }
-  });
-});
-
-router.post("/put", isLoggedIn, async (req, res) => {
-  const data = JSON.parse(req.body.data);
-  const id = data.id;
-  const params = {
-    TableName,
-    Key: { id },
-    UpdateExpression:
-      "set #n=:n, manufacturer=:m, description=:d, color=:c, price=:p, memory=:me, screen=:s, processor=:pro, ram=:r",
-    ExpressionAttributeNames: { "#n": "name" },
-    ExpressionAttributeValues: {
-      ":n": data.name,
-      ":m": data.manufacturer,
-      ":d": data.description,
-      ":c": data.color,
-      ":p": data.price,
-      ":me": data.memory,
-      ":s": data.screen,
-      ":pro": data.processor,
-      ":r": data.ram,
-    },
-  };
-
-  docClient.update(params, onUpdate);
-
-  function onUpdate(err, dat) {
-    if (err) {
-      console.error(
-        "Unable to update item. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-      res.send(new Error(err));
-    } else {
-      console.log("Updated item succeeded:", JSON.stringify(dat, null, 2));
-      res.send({ data, message: `Item with id=${id} updated successfully.` });
-    }
-  }
-});
+router.get("/", getAll);
+router.get("/pagination/:items/:exclusiveStartKey?", getAllPagination);
+router.get("/:id", getPhoneById);
+router.post("/", isLoggedIn, createPhone);
+router.post("/delete", isLoggedIn, deletePhoneById);
+router.post("/put", isLoggedIn, updatePhone);
 
 module.exports = router;
